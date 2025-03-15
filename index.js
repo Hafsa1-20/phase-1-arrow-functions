834012a55bd7240aafaebc90c3b2d01a499b2891
Hafsa <hafsa.abdikadir@student.moringaschool.com>
2025-03-15 04:25:55 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. Function expression for division
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // 2. Arrow function for squaring a number
+  const square = x => x * x;
+  
+  // 3. Arrow function for addition
+  const add = (a, b) => a + b;
+  
